e304bb213a1f61130c623e5b8b1ed369f75a11ea
Pratyush Ranjan <pratyush.ranjan@ringcentral.com>
2025-05-05 11:54:23 +0530

    Added search button

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -85,6 +85,7 @@ const SearchBox = () => {
                     <input ref={searchInput} className="w-full h-full p-2 outline-none" placeholder="Search through tags or text" onFocus={onFocusInSearch} onBlur={onFocusOutSearch} onChange={e => searchText.current=e.target.value} />
                     {focusInSearch && ( filterSelected ? filterOptionsList : filtersList )}
                 </div>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
             </div>
         </div>
     );
